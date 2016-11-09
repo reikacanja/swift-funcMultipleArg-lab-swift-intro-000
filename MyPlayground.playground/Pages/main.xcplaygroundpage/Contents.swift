bf0ef83cea6e4a791805e179bd3e36f7cd59c5cf
Rei Kacanja <reikacanja@Reis-MacBook-Pro.local>
2016-11-09 13:11:14 -0500

    Functions with multiple arguments Solutions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,8 +13,11 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func countriesInContinent(continent: String, numberOfCountries: Int) {
+        print("\(continent) is a continent which contains \(numberOfCountries) countries")
+    }
 
+countriesInContinent(continent: "Europe", numberOfCountries: 50)
 
 
 
@@ -25,31 +28,23 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting("Danny", "Hello")
-
-
-
-
+greeting(name: "Danny", greeting: "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
-daysInMonth("November", days: 30)
-
-
-
-
+daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func beverageSentence(beverageType: String, numberOfBeverages: Int, beverageLocation: String) {
+        print("\(numberOfBeverages) bottles of \(beverageType) on the \(beverageLocation).")
+    }
 
-
-
-
+beverageSentence(beverageType: "orange juice", numberOfBeverages: 12, beverageLocation: "fridge")
 /*:
  [Solution](solution)
  */
